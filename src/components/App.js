cffd828071912093aa50781ef261a01ab75ef0d0
Eugenia <mariaedallasta@gmail.com>
2021-02-24 10:12:20 +0100

    Add Style

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,11 @@ import pokemonArray from '../data/data.json';
 import PokemonList from './PokemonList';
 import '../styles/App.css';
 function App() {
-  console.log(pokemonArray);
   return (
     <div className="App">
       <h1>Pokemon List</h1>
-      <PokemonList itemsFromPokemonArray={pokemonArray}/>
-
-      
-    </div >
+      <PokemonList itemsFromPokemonArray={pokemonArray} />
+    </div>
   );
 }
 
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -6,7 +6,7 @@ function Pokemon(props) {
       <h3> Pokemon name: {props.name}</h3>
       <img src={props.url} alt={props.name} />
       <p>
-        Type {props.types[0]}-{props.types[1]}
+        Type {props.types[0]} {props.types[1]}
       </p>{" "}
     </li>
   );
